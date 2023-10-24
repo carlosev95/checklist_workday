0f8e6fad62933e523b22f7d3abf5d6ff39ddd604
Carlos Escobar Vazquez <cromo_lp@hotmail.com>
2023-10-24 08:37:19 -0600

    Separamos los scripts a un archivo independiente y cambiamos el placeholder del input Usuario

---
+++ js/checklist.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    // Inicializa el datepicker
+    $('#datepicker').datepicker({
+    format: 'dd MM yyyy', // Establece el formato deseado
+    language: 'es' // Cambia el idioma del datepicker a español
+    });
+
+    // Obtiene la fecha actual
+    var today = new Date();
+
+    // Establece la fecha actual en el datepicker
+    $('#datepicker').datepicker('setDate', today);
+});
+
+//Cambiar el alto del form de forma dinamica
+$('#myTextarea').on('input', function () {
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + 'px';
+});
